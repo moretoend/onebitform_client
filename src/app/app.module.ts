efa6aa214b956c68fbfd1c3bc085908bfe09092b
Daniel Moreto <dfmoreto@gmail.com>
2018-02-07 19:41:34 -0200

    Create Form and Answer modules and Home, Header, Footer and NotFound components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,31 @@ import { NgModule } from '@angular/core';
 import { MaterializeModule } from 'ng2-materialize';
 import { ColorPickerModule } from 'ngx-color-picker';
 import { ChartsModule } from 'ng2-charts';
+import { FormsModule } from './forms/forms.module';
+import { AnswersModule } from './answers/answers.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     MaterializeModule.forRoot(),
     ColorPickerModule,
-    ChartsModule
+    ChartsModule,
+    FormsModule,
+    AnswersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
