962be53934f10f04aef3ce85a22043c12014a4de
Daniel Moreto <dfmoreto@gmail.com>
2018-02-08 17:46:55 -0200

    Build FormForm component and render in FormEdit

--- src/app/forms/form-form/form-form.component.ts
+++ src/app/forms/form-form/form-form.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+import { MzToastService} from 'ng2-materialize';
+import { Router } from '@angular/router';
+
+import { FormService } from '../../shared/form.service';
+import { Form } from '../../shared/form.model';
+
 
 @Component({
   selector: 'app-form-form',
   templateUrl: './form-form.component.html',
   styleUrls: ['./form-form.component.css']
 })
+
 export class FormFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() form: Form;
 
-  ngOnInit() {
+  constructor(
+              private formService: FormService,
+              private toastService: MzToastService,
+              private router: Router
+             ) { }
+
+  ngOnInit() { }
+
+  onSubmit(f) {
+    if (this.form.id) {
+      this.formService.updateForm(this.form.id, this.form).subscribe(data => {
+        this.toastService.show('Form updated', 8000, 'green');
+      }, error => {
+        this.toastService.show('Problem in Form creation', 8000, 'red');
+      });
+    } else {
+      this.formService.createForm(this.form).subscribe(data => {
+        this.router.navigate(['/forms/' + data.slug]);
+      }, error => {
+        this.toastService.show('Problem in Form creation', 8000, 'red');
+      });
+    }
   }
-
 }
